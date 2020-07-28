7925139df4b78bc5fbacb283ea45f1fbed5b17ce
pedgomgal1 <pgomez-ibis@us.es>
2020-07-28 17:32:20 +0200

    polar scatters

--- code/mainPlots.m
+++ code/mainPlots.m
@@ -8,19 +8,20 @@ typeOfData = {'untilSR4_AdhesionA0FromWTGlands','untilSR4'};
 tissues = {'SalivaryGlandWT', 'Frusta', 'Voronoi'};
 colourBand = [0 1 0; 0 0 1; 0.5 0.5 0.5];
 numSR = 7;
-nRandomCells = 5;
+nRandomCells = 100;
 path2save = '..\results\graphics\individualCellsComparisons\';
 randomizationId = randi(100000,1);
 
 for nTypeData = 1 : length(typeOfData)
-    load(['..\results\' typeOfData{nTypeData} '_20_07_2020.mat'])
+    load(['..\results\' typeOfData{nTypeData} '_28-Jul-2020.mat'])
     %select random cells in frusta and Voronoi
     [randomCellTablesVoronoi, randomCellTablesFrusta] = selectRandomCellsInFrustaAdndVoronoi(nRandomCells,tableAllEnergies_Voronoi,tableAllEnergies_Frusta);
 
-    %track individual cells energies in frusta and Voronoi along SRs
-    compareEnergyPropertyIndividualCells(randomCellTablesVoronoi,randomCellTablesFrusta,numSR,colourBand,[path2save 'rand' num2str(randomizationId) '\'])
+%     %track individual cells energies in frusta and Voronoi along SRs
+%     compareEnergyPropertyIndividualCells(randomCellTablesVoronoi,randomCellTablesFrusta,numSR,colourBand,[path2save '\fewCells\rand' num2str(randomizationId) '\'])
     
-    %polar histogram length*adhesion - angle
+    %get general properties of the tissue
+    compareTissueProperties(randomCellTablesVoronoi,randomCellTablesFrusta,numSR,colourBand,[path2save '\generalTissue\rand' num2str(randomizationId) '\'])
     
     
 end
